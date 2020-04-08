4c8791bf8991962fe454c514b9435e50f2dd040b
Max <intelmax3@gmail.com>
2020-04-08 21:54:06 +0300

    Генератор публикаций

---
+++ src/service/cli/help.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const chalk = require(`chalk`);
+
+module.exports = {
+  name: '--help',
+  run() {
+    const text = `
+    Программа запускает http-сервер и формирует файл с данными для api.
+    Гайд:
+      server <command>
+      Команды:
+      --version:            выводит номер версии
+      --help:               печатает этот текст
+      --generate <count>    формирует файл mocks.json
+    `;
+
+    console.log(chalk.gray(text));
+  }
+};
